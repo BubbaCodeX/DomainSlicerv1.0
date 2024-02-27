18830c6654e56f81386bc55dcc0aa68052f5a825
BubbaCodeX <161252867+BubbaCodeX@users.noreply.github.com>
2024-02-27 18:43:04 -0500

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -21,8 +21,6 @@ var rootCmd = &cobra.Command{
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -33,14 +31,6 @@ func addSubCommands() {
 	rootCmd.AddCommand(network.ParseCmd)
 }
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.DomainSlicer_V1.1.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	addSubCommands()
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
